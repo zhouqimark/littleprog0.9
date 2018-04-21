f5bc9e4bf58bf029d8f7eeb2b17c7f51e928e3a1
mark <243345442@qq.com>
2018-04-21 19:16:19 +0800

    update

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = new Koa();
 const debug = require("debug")("little-prog");
 const response = require("./middlewares/response");
 const bodyParser = require("./middlewares/bodyparser");
-const router = reqiure("./routes");
+const router = require("./routes");
 const config = require("./config");
 
 app.use(response);
